9443187a69f1b4580a3f204e0efca166febc16a0
Satyam-R4j <satyamraj418@gmail.com>
2025-02-04 20:03:54 +0530

    Woking on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white flex justify-between  shadow-xl w-[85vw] fixed top-10 right-[10vw] rounded-full py-4 px-8 ">
       <div className="logo flex items-center gap-20 ">
-        <Image width={130} height={130} alt="logo" src="/logo.svg"></Image>
+       <a href="/"> <Image className="cursor-pointer" width={130} height={130} alt="logo" src="/logo.svg"></Image></a>
         <ul  className=" flex    cursor-pointer  text-gray-500 font-semibold    ">
             {["Templates","Marketplace","Discover","Pricing","Learn"].map((item,index)=>(
                 <li className=" hover:bg-slate-200 px-3 py-2 rounded-md"  key={index}>{item}</li>
@@ -13,7 +13,7 @@ const Navbar = () => {
         </ul></div>
         <div className="flex gap-2">
             <button className="login hover:bg-slate-300   font-semibold bg-slate-200 rounded-md p-4">Log in</button>
-            <button className="signup hover:bg-slate-700  font-semibold rounded-full bg-slate-900 text-white p-4">Sign up free</button>
+            <button  className="signup hover:bg-slate-700  font-semibold rounded-full bg-slate-900 text-white p-4">Sign up free</button>
         </div>
 
       
